61a3146773711bb69b06e7515d128fc1949d61c1
vicfriedman <victoria.m.friedman@gmail.com>
2016-02-25 13:44:44 -0500

    issue #1#

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -35,7 +35,7 @@ describe "nested hash" do
 
   describe "#adding_to_dennis" do 
     it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" do 
-      expect(adding_to_dennis[:dennis_ritchie][:languages]).to include("Assembly")
+      expect(adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
     end
   end
 end
