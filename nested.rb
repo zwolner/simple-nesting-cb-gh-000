8b7f5b5e6769dc9210b02b80c5f7711bcc643e4f
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-19 17:59:26 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -129,5 +129,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
