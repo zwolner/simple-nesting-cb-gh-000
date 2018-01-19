a63c2242e53d4558c4a7d35d80124fae2ccddc80
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-19 17:52:25 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:yukihiro_matsumoto] = {
+			 :known_for => "Ruby",
+			     :languages => ["LISP", "C"]
+			 }
 
 end
 
